d29bf983b3c793c6c74262518bd99c167f5b8fe4
Rossiel Gonzalez <alu0100763478@ull.edu.es>
2017-10-24 13:46:05 +0100

    Creación del método para devolver el valor energético que tiene un alimento para pasar la prueba #06.

--- alimento/lib/alimento.rb
+++ alimento/lib/alimento.rb
@@ -9,4 +9,9 @@ class Alimentos
         @nombre,@proteinas,@glucidos,@lipidos= nombre,proteinas,glucidos,lipidos
 
     end
+
+    def ve
+        @proteinas[0..@proteinas.size-1].to_f*4+@glucidos[0..@glucidos.size-1].to_f*4+@lipidos[0..@lipidos.size-1].to_f*9
+    end 
+
 end

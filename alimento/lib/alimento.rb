4a76dba1eb770fde3cb041538a7f59ae135fe9f0
Rossiel Gonzalez <alu0100763478@ull.edu.es>
2017-10-24 13:36:37 +0100

    Creación del método para devolver el nombre del alimento para pasar la prueba #02.

--- alimento/lib/alimento.rb
+++ alimento/lib/alimento.rb
@@ -2,4 +2,11 @@ require "alimento/version"
 
 class Alimentos
 
+    attr_reader :nombre
+    
+    def initialize(nombre,proteinas,glucidos,lipidos)
+        
+        @nombre,@proteinas,@glucidos,@lipidos= nombre,proteinas,glucidos,lipidos
+
+    end
 end

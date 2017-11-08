d0407111ce90c6c007244165d991653972789415
Rossiel Gonzalez <alu0100763478@ull.edu.es>
2017-11-08 10:22:36 +0000

    Creada la clase Lista para pasar la prueba #2

--- alimento/lib/lista.rb
+++ alimento/lib/lista.rb
@@ -1,1 +1,12 @@
 Node = Struct.new(:value, :next, :prev)
+
+
+class Lista
+    
+    attr_reader :cabeza,:cola
+    
+    def initialize()
+        @cabeza,@cola = nil
+    end
+    
+end
--- alimento/spec/alimento_spec.rb
+++ alimento/spec/alimento_spec.rb
@@ -44,25 +44,7 @@ RSpec.describe Alimentos do
         @nodo19 = Node.new(@platanos)  
 
         @lista = Lista.new
-        @lista.push(@nodo1)
-        @lista.push(@nodo2)
-        @lista.push(@nodo3)
-        @lista.push(@nodo4)
-        @lista.push(@nodo5)
-        @lista.push(@nodo6)
-        @lista.push(@nodo7)
-        @lista.push(@nodo8)
-        @lista.push(@nodo9)
-        @lista.push(@nodo10)
-        @lista.push(@nodo11)
-        @lista.push(@nodo12)
-        @lista.push(@nodo13)
-        @lista.push(@nodo14)
-        @lista.push(@nodo15)
-        @lista.push(@nodo16)
-        @lista.push(@nodo17)
-        @lista.push(@nodo18)
-        @lista.push(@nodo19)
+	
     end
 
     describe "Pruebas almacenamiento de las variables: " do
@@ -113,5 +95,6 @@ RSpec.describe Alimentos do
 	
    
 end
+
 
 

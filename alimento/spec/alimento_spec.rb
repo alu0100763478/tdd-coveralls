2b05d50a3a7ab75b456ce0c431cb900f4ecd40e4
Rossiel Gonzalez <alu0100763478@ull.edu.es>
2017-11-08 12:37:42 +0000

    Corrección: faltaba añadir contextos

--- alimento/spec/alimento_spec.rb
+++ alimento/spec/alimento_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Alimentos do
 
-    before :all do
-=begin
+context "# Alimentos" do
+before :all do
         @huevoFrito= Alimentos.new('Huevo frito','14.1g','0.0g','19.5g')
         @leche= Alimentos.new('Leche vaca','3.3g','4.8g','3.2g')
         @yogurt= Alimentos.new('Yogurt','3.8g','4.9g','3.8g')
@@ -22,9 +22,84 @@ RSpec.describe Alimentos do
         @tomate= Alimentos.new('Tomate', '1.0g','3.5g','0.2g')
         @cebolla= Alimentos.new('Cebolla','1.3g','5.8g','0.3g')
         @manzana= Alimentos.new('Manzana','0.3g','12.4g','0.4g')
-        @platanos= Alimentos.new('Plátano','1.2g','21.4g','0.2g')   
-=end
+        @platanos= Alimentos.new('Plátano','1.2g','21.4g','0.2g') 
+end 
 
+context "# Correcto almacenamiento y funcionamiento de los método de la clase Alimentos" do
+    describe "Pruebas almacenamiento de las variables: " do
+        it "Se almacena correctamente el nombre" do
+          expect(@yogurt.nombre).to eq('Yogurt') 
+          expect(@leche.nombre).to eq('Leche vaca')
+        end
+        it "Se almacena correctamente la cantidad de proteínas" do
+          expect(@platanos.proteinas).to eq('1.2g') 
+        end  
+        it "Se almacena correctamente la cantidad de glúcidos" do
+          expect(@manzana.glucidos).to eq('12.4g') 
+        end  
+        it "Se almacena correctamente la cantidad de lípidos" do
+          expect(@cebolla.lipidos).to eq('0.3g') 
+        end
+    end
+
+    describe "Prueba del método ve: " do
+        it "Se muestra el valor energético del alimento " do
+          expect(@tomate.ve).to eq(19.8)
+          expect(@chocolate.ve).to eq(479.2)
+          expect(@arroz.ve).to eq(343.4)
+          expect(@salmon.ve).to eq(202)
+        end
+    end
+
+    describe "Prueba del alimento formateado: " do
+        it "Se muestra el alimento formateado " do
+          expect(@papas.to_s).to eq("Alimento:\sPapas\n------------------------------\nProteínas:\s2.0g\nGlúcidos:\s15.4g\nLípidos:\s0.1g") 
+        end
+    end 
+end
+
+context "# Herencia, tipo de objeto y jerarquía de la clase Alimentos" do
+    describe "Pruebas para objeto alimentos" do
+        it "un objeto alimentos es una instancia de la clase Alimentos" do
+          expect(@huevoFrito).to be_an_instance_of(Alimentos::Alimentos)
+        end
+
+        it "un objeto alimentos es una instancia de la clase Object" do
+          expect(@huevoFrito).not_to be_an_instance_of(Object)
+        end
+
+        it "un objeto alimentos es una instancia de la clase BasicObject" do
+          expect(@huevoFrito).not_to be_an_instance_of(BasicObject)
+        end
+
+        it "un alientos es un Alimentos" do
+          expect(@huevoFrito).to be_a_kind_of(Alimentos::Alimentos)
+        end
+
+        it "un aliemntos es un Object" do
+          expect(@huevoFrito).to be_a_kind_of(Object)
+        end
+
+        it "un aliemntos es un BasicObject" do
+          expect(@huevoFrito).to be_a_kind_of(BasicObject)
+        end
+    end
+
+    describe "Pruebas para el tipo de un objeto Alimentos" do
+        it "un objeto alimentos responde al método nombre, proteinas,glucidos y lipidos" do
+          expect(@huevoFrito).to respond_to(:nombre,:proteinas,:glucidos,:lipidos)
+        end
+
+        it "un objeto alimentos responde al método ve" do
+          expect(@huevoFrito).to respond_to('ve')
+        end
+     end
+end    
+    
+end #context
+
+context "Listas y Grupos de Alimentos" do
+before :all do  
         @huevoFrito= AGrupos.new('Huevo frito','14.1g','0.0g','19.5g','Huevos, lácteos y helados')
         @leche= AGrupos.new('Leche vaca','3.3g','4.8g','3.2g','Huevos, lácteos y helados')
         @yogurt= AGrupos.new('Yogurt','3.8g','4.9g','3.8g','Huevos, lácteos y helados')
@@ -52,7 +127,7 @@ RSpec.describe Alimentos do
         @nodo5 = Node.new(@ternera)
         @nodo6 = Node.new(@pollo)
         @nodo7 = Node.new(@bacalo)
-        @nodo8 = Node.new(@atun)          
+        @nodo8 = Node.new(@atun)
         @nodo9 = Node.new(@salmon)
         @nodo10 = Node.new(@aceite)
         @nodo11 = Node.new(@chocolate)
@@ -62,9 +137,9 @@ RSpec.describe Alimentos do
         @nodo15 = Node.new(@papas)
         @nodo16 = Node.new(@tomate)
         @nodo17 = Node.new(@cebolla)
-        @nodo18 = Node.new(@manzana)          
-        @nodo19 = Node.new(@platanos)  
-
+        @nodo18 = Node.new(@manzana)
+        @nodo19 = Node.new(@platanos)
+        
         @lista = Lista.new
         @lista.push(@nodo1)
         @lista.push(@nodo2)
@@ -85,171 +160,112 @@ RSpec.describe Alimentos do
         @lista.push(@nodo17)
         @lista.push(@nodo18)
         @lista.push(@nodo19)
+end
 
-	@huevo= Alimentos.new('Huevo','14.1g','0.0g','19.5g') 	
+context "# Correcto almacenamiento y funcionamiento de los métodos de la clase Node" do
 
-    end
-=begin
-    describe "Pruebas almacenamiento de las variables: " do
-        it "Se almacena correctamente el nombre" do
-          expect(@yogurt.nombre).to eq('Yogurt') 
-          expect(@leche.nombre).to eq('Leche vaca')
-	end
-        it "Se almacena correctamente la cantidad de proteínas" do
-          expect(@platanos.proteinas).to eq('1.2g') 
-        end  
-        it "Se almacena correctamente la cantidad de glúcidos" do
-          expect(@manzana.glucidos).to eq('12.4g') 
-        end  
-        it "Se almacena correctamente la cantidad de lípidos" do
-          expect(@cebolla.lipidos).to eq('0.3g') 
-        end
-    end
-=end
-    describe "Prueba del método ve: " do
-        it "Se muestra el valor energético del alimento " do
-          expect(@tomate.ve).to eq(19.8) 
-	  expect(@chocolate.ve).to eq(479.2)
-	  expect(@arroz.ve).to eq(343.4)
-	  expect(@salmon.ve).to eq(202)	
-        end
-    end 
-=begin
-    describe "Prueba del alimento formateado: " do
-        it "Se muestra el alimento formateado " do
-          expect(@papas.to_s).to eq("Alimento:\sPapas\n------------------------------\nProteínas:\s2.0g\nGlúcidos:\s15.4g\nLípidos:\s0.1g") 
-        end
-    end 
-=end
-    describe "Prueba de nodos" do
-	it "Debe de existir un nodo con sus datos, siguiente y previo" do
-	  expect(@nodo1.value.nombre).to eq("Huevo frito")
-	  expect(@nodo2.value.nombre).to eq("Leche vaca")
-	  expect(@nodo10.value.nombre).to eq("Aceite de oliva")
+   describe "Prueba de nodos" do
+        it "Debe de existir un nodo con sus datos, siguiente y previo" do
+          expect(@nodo1.value.nombre).to eq("Huevo frito")
+          expect(@nodo2.value.nombre).to eq("Leche vaca")
+          expect(@nodo10.value.nombre).to eq("Aceite de oliva")
           expect(@nodo14.value.nombre).to eq("Lentejas")
-	  expect(@nodo11.value.nombre).to eq('Chocolate')
+          expect(@nodo11.value.nombre).to eq('Chocolate')
           expect(@nodo11.next.value.nombre).to eq('Azúcar')
           expect(@nodo12.value.nombre).to eq('Azúcar')
           expect(@nodo12.next.value.nombre).to eq('Arroz')
           expect(@nodo8.prev.value.nombre).to eq('Bacalao')
-          expect(@nodo9.prev.value.nombre).to eq('Atún')		
+          expect(@nodo9.prev.value.nombre).to eq('Atún')
           expect(@nodo3.value.nombre).to eq('Yogurt')
           expect(@nodo3.next.value.nombre).to eq('Cerdo')
           expect(@nodo3.prev.value.nombre).to eq('Leche vaca')
-
         end
     end
-
+end    
+    
+context "# Correcto almacenamiento y funcionamiento de los métodos de la clase Lista" do
     describe "Pruebas para la lista: " do
         it "Se inserta un nodo en la lista" do
-	  expect(@lista.cabeza.next.value.nombre).to eq('Leche vaca')
+          expect(@lista.cabeza.next.value.nombre).to eq('Leche vaca')
           expect(@lista.cabeza.prev.value.nombre).to eq('Plátano')
           expect(@lista.cabeza.value.nombre).to eq('Huevo frito')
           expect(@lista.cola.next.value.nombre).to eq('Huevo frito')
           expect(@lista.cola.prev.value.nombre).to eq('Manzana')
           expect(@lista.cola.value.nombre).to eq('Plátano')
-	end
+        end
 
-    	it "Se extrae el primer elemento de la Lista: " do
-      	  expect(@lista.cabeza.value.nombre).to eq('Huevo frito')
+        it "Se extrae el primer elemento de la Lista: " do
+          expect(@lista.cabeza.value.nombre).to eq('Huevo frito')
           @lista.pop_first
-      	  expect(@lista.cabeza.value.nombre).to eq('Leche vaca')
-      	end
-      	it "Se extrae el último elemento de la Lista: " do
-      	  expect(@lista.cola.value.nombre).to eq('Plátano')
+          expect(@lista.cabeza.value.nombre).to eq('Leche vaca')
+        end
+        it "Se extrae el último elemento de la Lista: " do
+          expect(@lista.cola.value.nombre).to eq('Plátano')
           @lista.pop_last
           expect(@lista.cola.value.nombre).to eq('Manzana')
-     	end      	
-    end			     
-	
-    describe "Pruebas para objeto alimentos" do     
-    	it "un objeto alimentos es una instancia de la clase Alimentos" do
-      	  expect(@huevo).to be_an_instance_of(Alimentos::Alimentos) 
-    	end
-    	
-	it "un objeto alimentos es una instancia de la clase Object" do
-       	  expect(@huevo).not_to be_an_instance_of(Object) 
-    	end
-
-    	it "un objeto alimentos es una instancia de la clase BasicObject" do
-       	  expect(@huevo).not_to be_an_instance_of(BasicObject) 
-    	end
-    
-    	it "un alientos es un Alimentos" do
-       	  expect(@huevo).to be_a_kind_of(Alimentos::Alimentos)
-    	end
-
-    	it "un aliemntos es un Object" do
-       	  expect(@huevo).to be_a_kind_of(Object) 
-    	end
-
-    	it "un aliemntos es un BasicObject" do
-       	  expect(@huevo).to be_a_kind_of(BasicObject)
-    	end
+        end
     end
+end
 
-    describe "Pruebas para objeto agrupos" do     
-    	it "un objeto agrupos es una instancia de la clase AGrupos" do 
-      	  expect(@huevoFrito).to be_an_instance_of(AGrupos::AGrupos) 
-    	end
-    
-	it "un objeto agrupos es una instancia de la clase Object" do
-       	  expect(@huevoFrito).not_to be_an_instance_of(Object) 
-    	end
+context "# Herencia, tipo y jerarquía de la clase AGrupos" do
+   describe "Pruebas para objeto agrupos" do
+        it "un objeto agrupos es una instancia de la clase AGrupos" do
+          expect(@huevoFrito).to be_an_instance_of(AGrupos::AGrupos)
+        end
 
-    	it "un objeto agrupos es una instancia de la clase BasicObject" do
-       	  expect(@huevoFrito).not_to be_an_instance_of(BasicObject) 
-    	end
-    
-   	it "un agrupos es un AGrupos" do
-       	  expect(@huevoFrito).to be_a_kind_of(AGrupos::AGrupos)
-    	end
+        it "un objeto agrupos es una instancia de la clase Object" do
+          expect(@huevoFrito).not_to be_an_instance_of(Object)
+        end
 
-    	it "un agrupos es un Object" do
-       	  expect(@huevoFrito).to be_a_kind_of(Object) 
-    	end
+        it "un objeto agrupos es una instancia de la clase BasicObject" do
+          expect(@huevoFrito).not_to be_an_instance_of(BasicObject)
+        end
 
-    	it "un agrupos es un BasicObject" do
-       	  expect(@huevoFrito).to be_a_kind_of(BasicObject)
-    	end
+        it "un agrupos es un AGrupos" do
+          expect(@huevoFrito).to be_a_kind_of(AGrupos::AGrupos)
+        end
+
+        it "un agrupos es un Object" do
+          expect(@huevoFrito).to be_a_kind_of(Object)
+        end
+
+        it "un agrupos es un BasicObject" do
+          expect(@huevoFrito).to be_a_kind_of(BasicObject)
+        end
     end
-
-    describe "Pruebas para el tipo de un objeto Alimentos" do      
-        it "un objeto alimentos responde al método nombre, proteinas,glucidos y lipidos" do
-       	  expect(@huevo).to respond_to(:nombre,:proteinas,:glucidos,:lipidos)
-    	end
-
-    	it "un objeto alimentos responde al método ve" do
-       	  expect(@huevo).to respond_to('ve')
-    	end
-     end  
-
-    describe "Pruebas para el tipo de un objeto AGrupos" do      
+    describe "Pruebas para el tipo de un objeto AGrupos" do
         it "un objeto agrupos responde al método nombre, proteinas,glucidos y lipidos" do
-       	  expect(@huevoFrito).to respond_to(:nombre,:proteinas,:glucidos,:lipidos)
-    	end
-        
-	it "un objeto agrupos responde al método nombre, proteinas,glucidos y lipidos" do
-       	  expect(@huevoFrito).to respond_to(:ngrupo)
-    	end
+          expect(@huevoFrito).to respond_to(:nombre,:proteinas,:glucidos,:lipidos)
+        end
 
-    	it "un objeto agrupos responde al método ve" do
-       	  expect(@huevoFrito).to respond_to('ve')
-    	end
-    end  
+        it "un objeto agrupos responde al método nombre, proteinas,glucidos y lipidos" do
+          expect(@huevoFrito).to respond_to(:ngrupo)
+        end
 
-    describe "Pruebas para la permanencia de una jerarquía" do      
-    	it "un agrupos es un Alimentos" do
-       	  expect(@huevoFrito).to be_a_kind_of(Alimentos::Alimentos)
-  	end
+        it "un objeto agrupos responde al método ve" do
+          expect(@huevoFrito).to respond_to('ve')
+        end
     end
 
-    describe "Pruebas para el formateo de los grupos de alimentos" do      
+    describe "Pruebas para la permanencia de una jerarquía" do
+        it "un agrupos es un Alimentos" do
+          expect(@huevoFrito).to be_a_kind_of(Alimentos::Alimentos)
+        end
+    end
+    
+end
+
+context "# Correcto formateo de la clase AGrupos" do
+    describe "Pruebas para el formateo de los grupos de alimentos" do
         it "Grupos de alimentos formateados" do
           expect(@papas.to_s).to eq("Grupo: Alimentos ricos en carbohidratos\nAlimento:\sPapas\n------------------------------\nProteínas:\s2.0g\nGlúcidos:\s15.4g\nLípidos:\s0.1g")
        end
-    end  	
+    end
 end
+  
+end #context  
 
 
 
+
+end

42baa70395a6a1d9676e73d3c484b9f499bd74f2
Rossiel Gonzalez <alu0100763478@ull.edu.es>
2017-11-29 11:01:05 +0000

    Creado el método ordenados_each para satisfacer la prueba anterior

--- alimento/lib/alimento/funcional.rb
+++ alimento/lib/alimento/funcional.rb
@@ -13,3 +13,20 @@ def ordenados_for(array)
   end
   list
 end
+
+def ordenados_each(array)
+  list = array.dup
+ # list = copia_ve(array)
+  return list if list.size <= 1 # already sorted
+  swapped = true
+  while swapped do
+    swapped = false
+    list.each_with_index   do |alimento,i|
+      if (i<list.size-1) && (list[i].ve > list[i+1].ve)
+        list[i], list[i+1] = list[i+1], list[i] # swap values
+        swapped = true
+      end
+    end    
+  end
+  list
+end    
